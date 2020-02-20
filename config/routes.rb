47f6a22d4c1e692e38d6aaca923f3cca9fe683fc
Godfrey18 <godfreymenezes07@gmail.com>
2020-02-20 17:30:23 +0530

    Test unit test functinal test integration

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ post 'login', to: 'sessions#create'
 
 delete 'logout', to: 'sessions#destroy'
 
+resources :categories, except: [:destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
