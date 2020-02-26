5ef70839111ef3e8675e2c409f06f8ba98d6d016
Godfrey18 <godfreymenezes07@gmail.com>
2020-02-26 11:44:46 +0530

    finshed and almost clear

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ApplicationRecord
 	belongs_to :user
 	has_many :article_categories
-    has_many :categories, through: :article_categories
+
+has_many :categories, through: :article_categories
+
 	validates :name, presence:true, uniqueness:true, length: {minimum:5,maximum:30}
 	validates :discription,presence:true,length: {minimum:6,maximum:100}
 	validates :user_id, presence:true
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,49 @@ validates :username, presence: true,
 
 uniqueness: { case_sensitive: false },
 
-length: { minimum: 3, maximum: 25 }
+length: { minimum: 8, maximum: 25 }
 
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-validates :email, presence: true, length: { maximum: 105 },
+validates :email, presence: true, length: {minimum: 12, maximum: 105 },
 
 uniqueness: { case_sensitive: false },
 
 format: { with: VALID_EMAIL_REGEX }
 
 has_secure_password
+
+PASSWORD_VALIDATOR = /(      # Start of group
+        (?:                        # Start of nonmatching group, 4 possible solutions
+          (?=.*[a-z])              # Must contain one lowercase character
+          (?=.*[A-Z])              # Must contain one uppercase character
+          (?=.*\W)                 # Must contain one non-word character or symbol
+        |                          # or...
+          (?=.*\d)                 # Must contain one digit from 0-9
+          (?=.*[A-Z])              # Must contain one uppercase character
+          (?=.*\W)                 # Must contain one non-word character or symbol
+        |                          # or...
+          (?=.*\d)                 # Must contain one digit from 0-9
+          (?=.*[a-z])              # Must contain one lowercase character
+          (?=.*\W)                 # Must contain one non-word character or symbol
+        |                          # or...
+          (?=.*\d)                 # Must contain one digit from 0-9
+          (?=.*[a-z])              # Must contain one lowercase character
+          (?=.*[A-Z])              # Must contain one uppercase character
+        )                          # End of nonmatching group with possible solutions
+        .*                         # Match anything with previous condition checking
+      )/x                          # End of group
+
+validates :password, {confirmation: true, presence: true, 
+	length: { minimum: 8 }, 
+	format: {
+        with: PASSWORD_VALIDATOR,
+        message: "must contain 3 of the following 4:
+         a lowercase letter, an uppercase letter, a digit,
+          a non-word character or symbol"
+        }
+    }
+
+
 
 end
